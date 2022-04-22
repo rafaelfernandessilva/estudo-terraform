ee246e471616a6a781483f14047e9d581e2d74f3
Deploy Action <action@github.com>
2022-04-22 14:18:24 +0000

    change image tag

--- terrafile.tf
+++ terrafile.tf
@@ -5,22 +5,22 @@ module "modulos" {
 }
 
 output "public_ip" {
-  value    = module.modulos.public_ip_east
+  value = module.modulos.public_ip_east
 
 }
 output "public_dns" {
-  value    = module.modulos.public_dns_east
+  value = module.modulos.public_dns_east
 }
 
 # Teste de retorno de um valor sensitive para nonsensitive
 variable "nosense" {
-  default = "MELHOR ENVIO"
-  type     = string
+  default   = "MELHOR ENVIO"
+  type      = string
   sensitive = true
 }
 
 output "teste_sense" {
-  value  = var.nosense
+  value     = var.nosense
   sensitive = true
 }
 
